43d1699edc876bff45c3adc22dce56d2f1f32473
aliousidib <alioubsidibe@gmail.com>
2023-03-13 20:46:59 +0000

    docker challenge

---
+++ docker-challenge/variables.tf
@@ -0,0 +1,18 @@
+variable "container_name" {
+  description = "name of the container"
+  type        = string
+  default     = "AltaResearchWebService"
+}
+
+variable "internal_port" {
+  description = "internal port to be used"
+  type        = number
+  default     = 9876
+}
+
+variable "external_port" {
+  description = "external port to be used"
+  type        = number
+  default     = 5432
+}
+
